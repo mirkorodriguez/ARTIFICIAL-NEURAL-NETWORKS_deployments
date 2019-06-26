893589883060223a345d67ee6ccd05bb4b12b470
Mirko Rodriguez <mirko.rodriguezm@gmail.com>
2019-06-25 20:00:16 -0500

    Adding script to convert pretrained models from Keras *.h5 to TensorFlow *.pb

--- deployment/tensor-serving/scripts/download_pretrained.py
+++ deployment/tensor-serving/scripts/download_pretrained.py
@@ -1,20 +1,21 @@
-import keras
+#Download and save pretrained models as keras models *.h5
 from keras.applications import vgg16, inception_v3, resnet50, mobilenet
 
-path_to_save = "../../../models/classification/images/pretrained/keras/"
+#path_to_save = "../../../models/classification/images/pretrained/keras/"
+path_to_save = "models/classification/images/pretrained/keras/"
 
 #Load and save the VGG model as *.h5 file
 vgg_model = vgg16.VGG16(weights='imagenet')
-vgg_model.save(''.join([path_to_save,'vgg.h5'])
+vgg_model.save(''.join([path_to_save,'vgg.h5']))
 
 #Load and save the Inception_V3 model as *.h5 file
 inception_model = inception_v3.InceptionV3(weights='imagenet')
-inception_model.save(''.join([path_to_save,'inception.h5'])
+inception_model.save(''.join([path_to_save,'inception.h5']))
 
 #Load and save the ResNet50 model as *.h5 file
 resnet_model = resnet50.ResNet50(weights='imagenet')
-resnet_model.save(''.join([path_to_save,'resnet.h5'])
+resnet_model.save(''.join([path_to_save,'resnet.h5']))
 
 #Load and save the MobileNet model as *.h5 file
 mobilenet_model = mobilenet.MobileNet(weights='imagenet')
-mobilenet_model.save(''.join([path_to_save,'mobilenet.h5'])
+mobilenet_model.save(''.join([path_to_save,'mobilenet.h5']))
---
+++ deployment/tensor-serving/scripts/keras2tensorflow.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+
+#path_to_load = "../../../models/classification/images/pretrained/keras/"
+path_to_load = "models/classification/images/pretrained/keras/"
+
+#path_to_save = "../../../models/classification/images/pretrained/tensorflow/"
+path_to_save = "models/classification/images/pretrained/tensorflow/"
+
+# The export path contains the name and the version of the model
+tf.keras.backend.set_learning_phase(0)  # Ignore dropout at inference
+model_name = 'inception'
+model = tf.keras.models.load_model(''.join([path_to_load,model_name,'.h5']))
+
+# Fetch the Keras session and save the model
+# The signature definition is defined by the input and output tensors
+# And stored with the default serving key
+with tf.keras.backend.get_session() as sess:
+     tf.saved_model.simple_save(sess,
+                                ''.join([path_to_save,model_name]),
+                                inputs={'input_image': model.input},
+                                outputs={t.name: t for t in model.outputs})

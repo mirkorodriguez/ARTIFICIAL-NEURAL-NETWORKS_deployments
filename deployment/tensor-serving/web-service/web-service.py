3be7bd522d3b3e8d9ebb3f08aa358da0bca9fd37
Mirko Rodriguez <mirko.rodriguezm@gmail.com>
2019-06-26 18:40:20 -0500

    Adding web-service REST file via Flask to hit TensorFlow Service.

--- deployment/tensor-serving/web-service/web-service.py
+++ deployment/tensor-serving/web-service/web-service.py
@@ -29,64 +29,64 @@ def port(x):
     }[x]
 
 def allowed_file(filename):
-	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 #Define a route
 @app.route('/')
 def default():
-	return 'TensorFlow Serving ... Go to /<model>/predict'
+    return 'TensorFlow Serving ... Go to /<model>/predict'
 
 @app.route('/inception/predict/',methods=['POST'])
 def predict():
 
-	model_name = "inception"
+    model_name = "inception"
 
-	data = {"success": False}
+    data = {"success": False}
 
-	if request.method == "POST":
-		# check if the post request has the file part
-		if 'file' not in request.files:
-			print('No file part')
-		file = request.files['file']
-		# if user does not select file, browser also submit a empty part without filename
-		if file.filename == '':
-			print('No selected file')
-		if file and allowed_file(file.filename):
-			filename = secure_filename(file.filename)
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    if request.method == "POST":
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            print('No file part')
+        file = request.files['file']
+        # if user does not select file, browser also submit a empty part without filename
+        if file.filename == '':
+            print('No selected file')
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-			#loading image
-			filename = UPLOAD_FOLDER + '/' + filename
-			print("filename:",filename)
+            #loading image
+            filename = UPLOAD_FOLDER + '/' + filename
+            print("filename:",filename)
 
-			img = image.img_to_array(image.load_img(filename, target_size=(224, 224)))
-			img = img.astype('float32')
+            img = image.img_to_array(image.load_img(filename, target_size=(224, 224)))
+            img = img.astype('float32')
 
-			payload = {"instances": [{'input_image': img.tolist()}]}
+            payload = {"instances": [{'input_image': img.tolist()}]}
 
-			# URI
-			uri = ''.join(['http://localhost:',port(model_name),'/v1/models/',model_name,':predict'])
-			print("URI:",uri)
+            # URI
+            uri = ''.join(['http://localhost:',port(model_name),'/v1/models/',model_name,':predict'])
+            print("URI:",uri)
 
-			# Request al modelo desplegado en TensorFlow Serving
-			r = requests.post(uri, json=payload)
-			pred = json.loads(r.content.decode('utf-8'))
+            # Request al modelo desplegado en TensorFlow Serving
+            r = requests.post(uri, json=payload)
+            pred = json.loads(r.content.decode('utf-8'))
 
-			# Decodificando decoder util
-			predictions = decode_predictions(np.array(pred['predictions']),top=1)
-			print("Predictions:\n",predictions)
-			label = predictions[0][0][1]
-			score = predictions[0][0][2]
+            # Decodificando decoder util
+            predictions = decode_predictions(np.array(pred['predictions']),top=1)
+            print("Predictions:\n",predictions)
+            label = predictions[0][0][1]
+            score = predictions[0][0][2]
 
-			#Results as Json
-			data["predictions"] = []
-			r = {"label": label, "score": float(score)}
-			data["predictions"].append(r)
+            #Results as Json
+            data["predictions"] = []
+            r = {"label": label, "score": float(score)}
+            data["predictions"].append(r)
 
-			#Success
-			data["success"] = True
+            #Success
+            data["success"] = True
 
-			return jsonify(data)
+            return jsonify(data)
 
 # Run de application
 app.run(host='0.0.0.0',port=5000)
